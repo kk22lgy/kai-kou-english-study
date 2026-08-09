b6256f619a70485b5bd730bd5281bb2c669305fa
kk22lgy <kk22lgy@gmail.com>
2026-08-09 22:13:07 +0800

    Add files via upload

---
+++ sw.js
@@ -0,0 +1,12 @@
+const CACHE = "kai-kou-v2";
+const BASE = new URL("./", self.registration.scope).pathname;
+const CORE = [BASE, `${BASE}manifest.webmanifest`, `${BASE}favicon.svg`];
+self.addEventListener("install", (event) => event.waitUntil(caches.open(CACHE).then((cache) => cache.addAll(CORE))));
+self.addEventListener("activate", (event) => event.waitUntil(caches.keys().then((keys) => Promise.all(keys.filter((key) => key !== CACHE).map((key) => caches.delete(key))))));
+self.addEventListener("fetch", (event) => {
+  if (event.request.method !== "GET") return;
+  event.respondWith(caches.match(event.request).then((cached) => cached || fetch(event.request).then((response) => {
+    if (response.ok && new URL(event.request.url).origin === self.location.origin) caches.open(CACHE).then((cache) => cache.put(event.request, response.clone()));
+    return response;
+  })));
+});
